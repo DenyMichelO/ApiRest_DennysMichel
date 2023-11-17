4775c77618c7d1f0250c837bc291d740e4d00726
DenyMichelO <dennys.michel@ucb.edu.bo>
2023-11-17 16:44:56 -0400

    Rest Api Ejercicio 1

---
+++ RestApi_E1/src/test/java/config/Configuration.java
@@ -0,0 +1,8 @@
+package config;
+
+public class Configuration {
+
+    public static String host="https://todo.ly";
+    public static String user="denymichel@ucb.edu.com";
+    public static String password="1025";
+}
